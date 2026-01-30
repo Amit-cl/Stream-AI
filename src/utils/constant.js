658da64882310745eff28e3e099fe02a1425cd49
Amit-cl <amitkumar2182004@gmail.com>
2026-01-30 23:30:36 +0530

    main home completed

--- src/utils/constant.js
+++
@@ -1,7 +0,0 @@
-export const options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NzI3MzUwYWJkM2JmNTI5ZmRlZWI4ZDZlNjVkNzI5NyIsIm5iZiI6MTc2OTc1NDUxOS40NjMsInN1YiI6IjY5N2M0Zjk3ZjYxYzdmMzA0M2ZlZTQ5YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Bez2HlwaD5XwcY-rhWP_WlHVqIIL8Z26dw_Tn1UVp54'
-  }
-};
